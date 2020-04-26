8c47c87e55b818183310ca3380b9b5bc8ba0685f
jinah0325 <jinah_0325@naver.com>
2020-04-26 13:35:21 +0900

    프로그램 메뉴 찍어주기

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,8 +7,25 @@ public class MainDrive {
 	public static void main(String[] args) {
 		
 //		전화 번호부 예제 시작 - 최진아
-				
+		printMenu();
+		
+		
 	}
+	
+	static void printMenu() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("***** 전화번호부 *****");
+		System.out.println("1. 전화번호 등록");
+		System.out.println("2. 전화번호 목록 조회");
+		System.out.println("0. 프로그램 종료");
+		System.out.println("*******************");
+		System.out.println("메뉴 선택 : ");
+		
+		int userInput = scan.nextInt();
+		
+	}
+	
+	
 }
 
 
