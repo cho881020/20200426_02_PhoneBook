d1a5b8cdab88afe9803012a408fddb74b081c065
조경진 <kj_cho@idealidea.co.kr>
2020-04-26 12:37:46 +0900

    다시 2020년으로 변경

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -11,8 +11,7 @@ public class MainDrive {
 		System.out.print("태어난 년도를 입력하세요 : ");
 		int birthYear = scan.nextInt();
 		
-//		2037년의 나이로 변경
-		int age = 2037 - birthYear +1;
+		int age = 2020 - birthYear +1;
 		System.out.println(age);
 		
 	}
