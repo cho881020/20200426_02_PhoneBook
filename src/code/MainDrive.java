f4ab65b231cd2ea35a897fb9de9c581e082fd3b5
jya1113 <64341529+jya1113@users.noreply.github.com>
2020-04-26 13:55:41 +0900

    폰번 추가화면 + 항목별 입력받기
    - scan.nextLine(); 으로 잔여 엔터키 대응

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -14,9 +14,9 @@ public class MainDrive {
 	}
 
 	static void printMenu() {
-		
-		while(true) {
-			
+
+		while (true) {
+
 			Scanner scan = new Scanner(System.in);
 			System.out.println("***** 전화번호부 *****");
 			System.out.println("1. 전화번호 등록");
@@ -24,28 +24,27 @@ public class MainDrive {
 			System.out.println("0. 프로그램 종료");
 			System.out.println("********************");
 			System.out.print("메뉴 선택 : ");
-		
+
 			int userInput = scan.nextInt();
-			
-			if(userInput == 0) {
+
+			if (userInput == 0) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
 			}
-			
-			else if(userInput == 1) {
-//				TODO - 전화번호 등록기능 수행
-				
-				
+
+			else if (userInput == 1) {
+				addPhoneNum();
+
 			}
-			
-			else if(userInput == 2) {
+
+			else if (userInput == 2) {
 //				TODO - 모든 전화번호 목록 출력
 			}
-			
+
 			else {
 				System.out.println("잘못된 입력입니다.");
 				System.out.println("1/2/0 중에서 입력해주세요.");
-				
+
 				try {
 //					프로그램 일시정지 코드(1초)
 					Thread.sleep(1000);
@@ -54,10 +53,29 @@ public class MainDrive {
 					e.printStackTrace();
 				}
 			}
-			
+
 		}
+
+	}
+
+	static void addPhoneNum() {
+		Scanner scan = new Scanner(System.in);
 		
+//		이름(String)/생년(int)/전화번호(String)
 		
+		System.out.println("*** 전화번호 등록 ***");
+		System.out.print("이름 입력 : ");
+		String inputName = scan.nextLine();
+		
+		System.out.print("생년 : ");
+		int inputYear = scan.nextInt();
+//		남아있는 엔터키를 제거하기위한 문장.
+		scan.nextLine();
+		
+		System.out.print("전화번호 : ");
+		String inputPhone = scan.nextLine();
+		
+		System.out.println(String.format("%s / %d / %s", inputName, inputYear, inputPhone));
 	}
 
 }
