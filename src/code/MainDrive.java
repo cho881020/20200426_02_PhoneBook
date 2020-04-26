ad1ca60b858f914b14f3f22c64c862658b0c4e7d
jinah0325 <jinah_0325@naver.com>
2020-04-26 13:43:16 +0900

    메뉴 선택 기능 일부 구현
    - 0번 입력시 프로그램 종료
    - 잘못 입력한 경우 안내메세지 출력
    - 1초간 정지 후 메뉴 재 출력(Thread)

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,21 +7,47 @@ public class MainDrive {
 	public static void main(String[] args) {
 		
 //		전화 번호부 예제 시작 - 최진아
-		printMenu();
-		
-		
+		printMenu();		
 	}
 	
 	static void printMenu() {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("***** 전화번호부 *****");
-		System.out.println("1. 전화번호 등록");
-		System.out.println("2. 전화번호 목록 조회");
-		System.out.println("0. 프로그램 종료");
-		System.out.println("*******************");
-		System.out.println("메뉴 선택 : ");
 		
-		int userInput = scan.nextInt();
+		while (true) {
+			
+			Scanner scan = new Scanner(System.in);
+			System.out.println("***** 전화번호부 *****");
+			System.out.println("1. 전화번호 등록");
+			System.out.println("2. 전화번호 목록 조회");
+			System.out.println("0. 프로그램 종료");
+			System.out.println("*******************");
+			System.out.print("메뉴 선택 : ");
+			
+			int userInput = scan.nextInt();
+			
+			if (userInput == 0) {
+				System.out.println("프로그램을 종료 합니다.");
+				break;
+			}
+			else if (userInput == 1) {
+//				TODO - 전화번호 등록 기능 수행				
+			}
+			else if (userInput == 2) {
+//				TODO - 모든 전화번호 목록 출력
+			}
+			else {
+				System.out.println("잘못된 입력입니다.");
+				System.out.println("1/2/0 중에 입력해주세요.");
+				
+				try {
+//					프로그램 일시정지 코드 (1초)
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}			
+			
+		}
 		
 	}
 	
