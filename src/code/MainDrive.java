86704d5d6c85fd8780a26c5c39cd0a0510d42c8b
조경진 <kj_cho@idealidea.co.kr>
2020-04-26 12:44:56 +0900

    내용 초기화

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -6,13 +6,7 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
-//		사용자의 생년을 입력받아서 > 나이로 변환.
-		Scanner scan = new Scanner(System.in);
-		System.out.print("태어난 년도를 입력하세요 : ");
-		int birthYear = scan.nextInt();
-		
-		int age = 2020 - birthYear +1;
-		System.out.println(age);
+//		전화 번호부 예제 시작
 		
 	}
 }
