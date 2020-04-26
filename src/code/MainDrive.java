26495420711a3b7dd472c239d9e46940a5d92f04
jya1113 <64341529+jya1113@users.noreply.github.com>
2020-04-26 13:35:22 +0900

    프로그램 메뉴 찍어주기

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,9 +7,25 @@ public class MainDrive {
 	public static void main(String[] args) {
 
 //		전화 번호부 예제 시작 - 정윤아
-		
 
+//		프로그램 시작시 메뉴 출력
+		printMenu();
+
+	}
+
+	static void printMenu() {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("***** 전화번호부 *****");
+		System.out.println("1. 전화번호 등록");
+		System.out.println("2. 전화번호 목록 조회");
+		System.out.println("0. 프로그램 종료");
+		System.out.println("********************");
+		System.out.print("메뉴 선택 : ");
+	
+		int userInput = scan.nextInt();
 		
 		
 	}
+
 }
