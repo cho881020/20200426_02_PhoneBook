e2cc97a1c8e10cc460b7a687591133d64a804528
kwon0426 <64341865+kwon0426@users.noreply.github.com>
2020-04-26 13:43:21 +0900

    메뉴선택 기능 일부 구현
    - 0번 입력시 프로그램 종료
    - 잘못 입력한 경우 안내 메세지 출력
    - 1초간 정지후 메뉴 다시 출력 Thread

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -12,15 +12,41 @@ public class MainDrive {
 	
 	static void printMenu() {
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("***** 전화번호부 *****");
-		System.out.println("1. 전화번호 등록");
-		System.out.println("2. 전화번호 목록 조회");
-		System.out.println("0. 프로그램 종료");
-		System.out.println("*******************");
-		System.out.print("메뉴 선책 : ");
-		
-		int usseInput = scan.nextInt();
+		while (true) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("***** 전화번호부 *****");
+			System.out.println("1. 전화번호 등록");
+			System.out.println("2. 전화번호 목록 조회");
+			System.out.println("0. 프로그램 종료");
+			System.out.println("*******************");
+			System.out.print("메뉴 선책 : ");
+			
+			int userInput = scan.nextInt();
+			
+			if (userInput == 0 ) {
+				System.out.println("프로그램을 종료 합니다.");
+				break;
+			}
+			else if(userInput == 1) {
+//				TODO - 전화번호 등록 기능 수행
+			}
+			else if(userInput == 2) {
+//				TODO - 모든 전화번호 목록 출력
+			}
+			else {
+				System.out.println("잘못된 입력 입니다.");
+				System.out.println("1/2/0 중에서 입력해주세요");
+				
+				try {
+//					프로그램을 일시 정시 코드(1초)
+					Thread.sleep(1000);  // 1초 동안 정지
+				} catch (InterruptedException e) {
+//					 TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
 		
 	}
 }
